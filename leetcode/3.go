3d2976956b445ba7d9e2a8cbe1e9aef5d1aa06ab
chenhao <563278383@qq.com>
2018-04-04 13:25:45 +0800

    + leetcode 3

---
+++ leetcode/3.go
@@ -0,0 +1,161 @@
+/*
+	Longest Substring Without Repeating Characters 无重复字符的最长子串
+
+	思路:
+	假设L[i] = s[start … i]，表示最长的子字符串，其中没有重复的元素，我们以map保存< 字符，索引>；然后访问s[i + 1]：
+		1）如果s[i + 1]没有出现在map中，我们可以将s[i + 1]添加到map中，则有L[i + 1] = s[start … i + 1]；
+		2）如果s[i + 1]存在于map中，并且map中对应的索引是k；则令start = max(start，k)，则L[i + 1] = s[start … i + 1]，记录此时的最大长度；
+
+		start = max(start，k)是为了避免start出现回退,比如"abba"的最后的字符"a"
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	Test()
+	Test2()
+	Test3()
+	Test4()
+	Test5()
+	Test6()
+}
+
+func Test() {
+	s := "abcabcbb"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 3)
+}
+
+func Test2() {
+	s := "bbbbb"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 1)
+}
+
+func Test3() {
+	s := "pwwkew"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 3)
+}
+
+func Test4() {
+	s := "a"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 1)
+}
+
+func Test5() {
+	s := "aabab"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 2)
+}
+
+func Test6() {
+	s := "abba"
+
+	n := lengthOfLongestSubstring3(s)
+	fmt.Println(s, n, n == 2)
+}
+
+func printBytes(cs []byte) {
+	for _, v := range cs {
+		fmt.Printf("%c", v)
+	}
+
+	fmt.Println()
+}
+
+// 最笨
+// O（n ^ 2）
+func lengthOfLongestSubstring(s string) int {
+	cs := []byte(s)
+	l := len(cs)
+	if l == 0 {
+		return 0
+	}
+
+	max := 1 // Test4
+
+	for i := 0; i < l; i++ {
+		m := make(map[byte]struct{})
+		m[cs[i]] = struct{}{}
+
+		for j := i + 1; j < l; j++ {
+			if _, ok := m[cs[j]]; !ok {
+				m[cs[j]] = struct{}{}
+			} else {
+				if len(m) > max {
+					max = len(m)
+				}
+
+				break
+			}
+
+			if j == l-1 && len(m) > max { // for Test5
+				max = len(m)
+			}
+		}
+	}
+
+	return max
+}
+
+// 用v数组记录字母出现的位置，start指上次子串查找中, 重复出现的那个字符的位置即(本次查找子串的第一个字符的前一个字符)???
+// O（n）
+func lengthOfLongestSubstring2(s string) int {
+	var a [128]int
+	for i := range a { // 排除a[i]为0的影响
+		a[i] = -1
+	}
+
+	max, start := 0, -1
+
+	for i, v := range s {
+		//if a[v] > -1 { // for Test6
+		if a[v] > start { // 本次出现的位置a[v]应在满足上一次出现重复字符的位置(start)的后面
+			start = a[v] // 上一次v出现时的index 对应下面的`a[v] = i`
+		}
+
+		a[v] = i // 当前v出现的index
+
+		if i-start > max {
+			max = i - start
+		}
+
+		fmt.Println(v, a[v], start)
+	}
+
+	return max
+}
+
+// 最好理解
+// O（n）
+func lengthOfLongestSubstring3(s string) int {
+	max, start := 0, 0 // start为0是指从s[0]开始查找
+	m := make(map[rune]int)
+
+	for i, v := range s {
+		j1, ok1 := m[v]
+		fmt.Println(j1, ok1, start, j1)
+		// start <= j : ???                      // example: "aab" for start == j
+		if j, ok := m[v]; ok && start <= j {
+			start = j + 1 // 从上一次出现重复字符的下一个位置开始重新计算
+		} else {
+			if i-start+1 > max { // for 求max
+				max = i - start + 1
+			}
+		}
+
+		m[v] = i
+	}
+
+	return max
+}
