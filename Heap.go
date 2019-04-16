4be244882f509e8288157ad7f0161db14d47ef24
meilihao <563278383@qq.com>
2019-04-16 20:22:46 +0800

    f 三个数比较大小

--- Heap.go
+++ Heap.go
@@ -47,6 +47,17 @@ func (h *Heap) RemoveMax() {
 	heapify(h.a, h.count, 1)
 }
 
+func (h *Heap) Sort() {
+	k := h.count
+
+	for k > 1 {
+		h.a[1], h.a[k] = h.a[k], h.a[1]
+		k--
+
+		heapify(h.a, k, 1)
+	}
+}
+
 func main() {
 	nums := []int{0, 7, 5, 19, 8, 4, 1, 20, 13, 16}
 	fmt.Println(nums)
@@ -56,7 +67,8 @@ func main() {
 	// 	h.Insert(v)
 	// }
 	// fmt.Println(h.a[1 : h.count+1])
-	// h.RemoveMax()
+	// //h.RemoveMax()
+	// h.Sort()
 	// fmt.Println(h.a[1 : h.count+1])
 
 	buildHead(nums, len(nums)-1)
@@ -84,7 +96,7 @@ func heapify(a []int, n, i int) {
 		if i*2 <= n && a[i] < a[i*2] {
 			maxPos = i * 2
 		}
-		if i*2+1 <= n && a[i] < a[i*2+1] {
+		if i*2+1 <= n && a[maxPos] < a[i*2+1] { // 三个数比较大小, 这里是a[maxPos]而不是a[i]
 			maxPos = i*2 + 1
 		}
 		if maxPos == i {
