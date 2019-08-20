42b74cba9d895d4a967a2341a078ec953d4daa3d
meilihao <563278383@qq.com>
2019-08-20 20:04:05 +0800

    .

--- leetcode/234.go
+++ leetcode/234.go
@@ -8,31 +8,31 @@ type ListNode struct {
 }
 
 func main() {
-	s := "abc"
+	s := "aba"
 	head := BuildList(s)
 
 	fmt.Println(isPalindrome(head))
 }
 
-// func BuildList(s string) *ListNode {
-// 	var head, pre, tmp *ListNode
+func BuildList(s string) *ListNode {
+	var head, pre, tmp *ListNode
 
-// 	for _, v := range s {
-// 		tmp = &ListNode{
-// 			Val: int(v),
-// 		}
+	for _, v := range s {
+		tmp = &ListNode{
+			Val: int(v),
+		}
 
-// 		if pre != nil {
-// 			pre.Next = tmp
-// 			pre = tmp
-// 		} else {
-// 			pre = tmp
-// 			head = tmp
-// 		}
-// 	}
+		if pre != nil {
+			pre.Next = tmp
+			pre = tmp
+		} else {
+			pre = tmp
+			head = tmp
+		}
+	}
 
-// 	return head
-// }
+	return head
+}
 
 // slow:fast => fast=2*slow-1
 func isPalindrome(head *ListNode) bool {
@@ -43,17 +43,17 @@ func isPalindrome(head *ListNode) bool {
 	slow := head
 	fast := head
 
-	var pre, tmp *ListNode // 用于翻转链表
+	var pre, next *ListNode // 用于翻转链表
 
 	for fast != nil && fast.Next != nil { // 确保fast能跳两格
 		fast = fast.Next.Next
 
-		tmp = slow.Next
+		next = slow.Next
 
 		slow.Next = pre //翻转链表方向
 
 		pre = slow
-		slow = tmp
+		slow = next
 	}
 
 	if fast != nil { // 链表长度为奇数, 画图可清晰了解
