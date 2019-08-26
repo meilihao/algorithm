b08b878446377f49542e3498d307eb41c1c47360
meilihao <563278383@qq.com>
2019-08-26 09:50:58 +0800

    + leetcode 122

---
+++ leetcode/122.go
@@ -0,0 +1,26 @@
+// 122. 买卖股票的最佳时机 II
+// 思路:
+// 1. 贪心
+// 2. dp(动态规划)
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{7, 1, 5, 3, 6, 4}
+
+	fmt.Println(maxProfit(nums))
+}
+
+// 贪心
+func maxProfit(prices []int) int {
+	res := 0
+
+	for i := 1; i < len(prices); i++ { // 已能排除len(prices) < 2的情况
+		if prices[i] > prices[i-1] { // 只要当天的股价比前一天高就买卖一次
+			res += prices[i] - prices[i-1]
+		}
+	}
+
+	return res
+}
