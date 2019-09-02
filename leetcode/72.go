a16355832fc6354e4d08bf12d6f3c6e071642c50
meilihao <563278383@qq.com>
2019-09-02 15:52:22 +0800

    .

--- leetcode/72.go
+++ leetcode/72.go
@@ -51,7 +51,7 @@ func minDistance(A string, B string) int {
 			if bs[i-1] == as[j-1] {
 				dp[i][j] = dp[i-1][j-1]
 			} else {
-				dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
+				dp[i][j] = dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1 // min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+1)
 			}
 		}
 	}
