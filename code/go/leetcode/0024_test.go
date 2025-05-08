efbddf02a59b643310d5d27568ae7575c05261c8
meilihao <563278383@qq.com>
2025-05-08 13:53:30 +0800

    chore(u): 0508

--- code/go/leetcode/0024_test.go
+++ code/go/leetcode/0024_test.go
@@ -39,7 +39,34 @@ func swapPairs(head *ListNode) *ListNode {
 	return dummyHead.Next
 }
 
+// 两个临时变量cur,next
+func swapPairs_1(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	dummyHead := &ListNode{ // 哨兵节点
+		Next: head,
+	}
+
+	pre := dummyHead
+	var cur, next *ListNode
+
+	for pre.Next != nil && pre.Next.Next != nil { // 有元素需要交换
+		cur = pre.Next
+		next = cur.Next
+
+		pre.Next = next
+		cur.Next = next.Next
+		next.Next = cur
+		pre = cur
+	}
+
+	return dummyHead.Next
+}
+
 // pre ->1->2->3 -> pre->2->1->3 : 逆推: 因为要修改1.next, 而原1.next指向2, 所以要暂存2
+// 一个临时变量tmp
 func swapPairs3(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
